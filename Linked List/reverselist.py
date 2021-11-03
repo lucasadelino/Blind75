735e4bf7da2fd42dcce1fbce5e48a839ce4771ce
Lucas Adelino <lcsadelino@gmail.com>
2021-11-03 19:08:24 -0400

    Updated comments

--- Linked List/reverselist.py
+++ Linked List/reverselist.py
@@ -1,16 +1,24 @@
-# Definition for singly-linked list.
+# Definition for singly-linked provided by LeetCode
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-# I believe this is the recursive solution?
+# Iterative solution
 class Solution:
     def reverseList(self, head):
+        """
+        Iterates through the list, saving references to the previous element,
+        and setting the .next of the current element to the previous element.
+        """
         pointer = head
         prev = None
         while pointer is not None:
+            # Create a new reference to pointer.next, because we will change 
+            # where pointer.next points to, but we also need the original 
+            # reference to continue to iterate
             oldnext = pointer.next
+            
             pointer.next = prev
             prev = pointer
             pointer = oldnext

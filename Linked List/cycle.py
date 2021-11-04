7fb9551e2604d885f587e35eb8cfa638c38cda61
Lucas Adelino <lcsadelino@gmail.com>
2021-11-04 12:03:14 -0400

    Fixed none pointer check order

--- Linked List/cycle.py
+++ Linked List/cycle.py
@@ -9,15 +9,16 @@ class Solution:
         """Use a set to keep track of object ids, and a counter that increases
         everytime a node is visited. If there is a discrepancy between the
         counter and the length of set elements, it means we just tried to add 
-        the ID of a node we already visited."""
+        the ID of a node."""
         setids = set()
         pointer = head
         counter = 0
         while True:
+            if pointer is None:
+                return False
+                
             setids.add(id(pointer))
             counter += 1
             if counter > len(setids):
                 return True
             pointer = pointer.next
-            if pointer is None:
-                return False

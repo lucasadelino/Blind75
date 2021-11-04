dc67a34ec56f11d564cc120d496b4a09695eaf0a
Lucas Adelino <lcsadelino@gmail.com>
2021-11-04 12:20:58 -0400

    Second submission; No counter

--- Linked List/cycle.py
+++ Linked List/cycle.py
@@ -6,19 +6,16 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: ListNode) -> 'bool':
-        """Use a set to keep track of object ids, and a counter that increases
-        everytime a node is visited. If there is a discrepancy between the
-        counter and the length of set elements, it means we just tried to add 
-        the ID of a node."""
-        setids = set()
+        """Use a set to keep track of object ids. If we try to add another 
+        element, then there is a cycle"""
+        objset = set()
         pointer = head
-        counter = 0
         while True:
             if pointer is None:
                 return False
-                
-            setids.add(id(pointer))
-            counter += 1
-            if counter > len(setids):
+            if pointer in objset:
                 return True
+            else:
+                objset.add(pointer)
             pointer = pointer.next
+            

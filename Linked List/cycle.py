7204590e0641c3db20651de900e9c1f8f8f0ad3c
Lucas Adelino <lcsadelino@gmail.com>
2021-11-04 11:55:33 -0400

    First commit

---
+++ Linked List/cycle.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list provided by LeetCode
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> 'bool':
+        """Use a set to keep track of object ids, and a counter that increases
+        everytime a node is visited. If there is a discrepancy between the
+        counter and the length of set elements, it means we just tried to add 
+        the ID of a node we already visited."""
+        setids = set()
+        pointer = head
+        counter = 0
+        while True:
+            setids.add(id(pointer))
+            counter += 1
+            if counter > len(setids):
+                return True
+            pointer = pointer.next
+            if pointer is None:
+                return False
